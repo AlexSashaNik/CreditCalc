55d0451f51eee505677509efe54d959ef50f7cdd
AlexNikitina <alex.nikitina82@gmail.com>
2023-05-26 13:31:55 +0200

    Readme changed

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,16 +1,9 @@
 public class CreditPaymentService {
     public double calculate(double creditAmount, double creditPercentPerYear, int yearNum) {
-        if (creditAmount < 0 || creditPercentPerYear < 0 || yearNum < 0 || yearNum > 3) {
-            creditAmount = 0;
-        }
-
-        int monthNum = yearNum * 12;
         double creditPercentPerMonth = creditPercentPerYear / 12 / 100;
-        double creditPercentPerMonth1 = 1 + creditPercentPerMonth;
-        double intoBrackets = Math.pow(creditPercentPerMonth1, monthNum);
-        double upperBound = creditPercentPerMonth * intoBrackets;
-        double bottomBound = intoBrackets - 1;
-        return creditAmount * upperBound / bottomBound;
+        double result = creditPercentPerMonth * Math.pow((1 + creditPercentPerMonth), yearNum) / (Math.pow(1 + creditPercentPerMonth, yearNum) - 1);
+        result = result * creditAmount;
+        return (int) result;
     }
 
 }
